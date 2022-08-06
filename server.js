49d7dbc4fbf9d41ad60bf83749ed7df3ebe6fe22
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-06 14:58:13 +0200

    Add endpoints

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
 
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
 
 const app = express();
+
+app.engine('hbs', hbs());
+
+app.set('view engine', '.hbs');
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
 
+app.get('/testimonials', (req, res) => {
+  res.render('db', { layout: false })
+});
 
+app.get('/testimonials/:id', (req, res) => {
+  res.render('db', { layout: false, id: req.params.id })
+});
+
+app.get('/testimonials/random', (req, res) => {
+  res.render('db', { layout: false })
+});
+
+app.post('/testimonials', (req, res) => {
+  res.render('db', { layout: false })
+});
+
+app.put('/testimonials/:id', (req, res) => {
+  res.render('db', { layout: false, id: req.params.id })
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+  res.render('db', { layout: false, id: req.params.id })
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
 
 
 app.listen(8000, () => {
