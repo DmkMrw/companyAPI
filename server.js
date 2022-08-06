1333dadd9673f1d6c98198208ef1e298a47cb5e0
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-06 13:53:05 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+];
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+
+
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
