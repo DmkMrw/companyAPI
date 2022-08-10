d7d4c5c7a0dc5007ae4d0421cf0a4f9ac817bb6f
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-10 14:24:20 +0200

    Add routes for testimonials

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
 const uuid = require('uuid').v4;
+const cors = require('cors');
 const db = require('./db')
 
-//     const db = [
-//       { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-//       { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-//       { id: 3, author: 'Paul Doe', text: 'Vel blanditiis hic tempora ab culpa.' },
-//       { id: 4, author: 'Sean Doe', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.' },
-// ];
+//import routes
+const testimonialRoutes = require('./routes/testimonials.routes.js')
 
 
 const app = express();
@@ -18,48 +15,15 @@ app.engine('hbs', hbs());
 
 app.set('view engine', '.hbs');
 
-const testimonials = db.testimonials;
 const concerts = db.concerts;
 const seats = db.seats;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
-// TESTIMONIALS
-app.get('/testimonials', (req, res) => {
-  res.json(testimonials)
-});
+app.use('/api/', testimonialRoutes);
 
-app.get('/testimonials/random', (req, res) => {
-  res.json(testimonials[Math.floor(Math.random() * db.length)]);
-});
-
-app.get('/testimonials/:id', (req, res) => {
-  res.json(testimonials[req.params.id-1])
-});
-
-app.post('/testimonials', (req, res) => {
-  const { author, text } = req.body;
-  const id = uuid();
-  const newTestimonials = { id, author, text };
-  testimonials.push(newTestimonials);
-  res.json({ message: 'OK' });
-});
-
-app.put('/testimonials/:id', (req, res) => {
-  const { author, text } = req.body;
-  const id = req.params.id-1;
-  const testimonial = testimonials[id];
-  testimonial.author = author;
-  testimonial.text = text;
-  res.json({ message: 'OK' });
-});
-
-app.delete('/testimonials/:id', (req, res) => {
-  const id = req.params.id-1;
-  testimonials.splice(db[id], 1);
-  res.json({ message: 'OK' });
-});
 
 // CONCERTS
 app.get('/concerts', (req, res) => {
