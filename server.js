06b15012cc01f070c53276dc626b3f35e3c9a725
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-10 16:25:01 +0200

    Prepare to Heroku

--- server.js
+++ server.js
@@ -13,16 +13,21 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(cors());
 
 app.use('/api/', testimonialRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json('404 not found...');
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
