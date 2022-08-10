f2131d145d5174a6e81a632991480c3c5b83eaec
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-10 14:33:02 +0200

    Add routes for seats

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const path = require('path');
 const uuid = require('uuid').v4;
 const cors = require('cors');
-const db = require('./db')
+const db = require('./db');
 
 //import routes
-const testimonialRoutes = require('./routes/testimonials.routes')
-const concertRoutes = require('./routes/concerts.routes')
-
+const testimonialRoutes = require('./routes/testimonials.routes');
+const concertRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
-
-const seats = db.seats;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -19,49 +17,11 @@ app.use(cors());
 
 app.use('/api/', testimonialRoutes);
 app.use('/api', concertRoutes);
-
-
-
-
-// SEATS
-app.get('/seats', (req, res) => {
-  res.json(seats)
-});
-
-app.get('/seats/:id', (req, res) => {
-  res.json(seats[req.params.id-1])
-});
-
-app.post('/seats', (req, res) => {
-  const { day, seat, client, email } = req.body;
-  const id = uuid();
-  const newSeat = { id, day, seat, client, email };
-  seats.push(newSeat);
-  res.json({ message: 'OK' });
-});
-
-app.delete('/seats/:id', (req, res) => {
-  const id = req.params.id-1;
-  seats.splice(seats[id], 1);
-  res.json({ message: 'OK' });
-});
-
-app.put('/seats/:id', (req, res) => {
-  const { day, seat, client, email } = req.body;
-  const id = req.params.id-1;
-  const editSeat = seats[id];
-  editSeat.day = day;
-  editSeat.seat = seat;
-  editSeat.client = client;
-  editSeat.email = email;
-  res.json({ message: 'OK' });
-});
-
+app.use('/api', seatsRoutes);
 
 app.use((req, res) => {
   res.status(404).json('404 not found...');
 })
-
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
