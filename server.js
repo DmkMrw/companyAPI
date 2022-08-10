a28cc7d0319465b37ec468d7e8a2d7c51e9f8bd5
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-10 14:10:50 +0200

    Add endpoint for concerts and seats

--- server.js
+++ server.js
@@ -19,10 +19,13 @@ app.engine('hbs', hbs());
 app.set('view engine', '.hbs');
 
 const testimonials = db.testimonials;
+const concerts = db.concerts;
+const seats = db.seats;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// TESTIMONIALS
 app.get('/testimonials', (req, res) => {
   res.json(testimonials)
 });
@@ -56,8 +59,77 @@ app.delete('/testimonials/:id', (req, res) => {
   const id = req.params.id-1;
   testimonials.splice(db[id], 1);
   res.json({ message: 'OK' });
-
 });
+
+// CONCERTS
+app.get('/concerts', (req, res) => {
+  res.json(concerts)
+});
+
+app.get('/concerts/:id', (req, res) => {
+  res.json(concerts[req.params.id-1])
+});
+
+app.post('/concerts', (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const id = uuid();
+  const newConcert = { id, performer, genre, price, day, image };
+  concerts.push(newConcert);
+  res.json({ message: 'OK' });
+});
+
+app.delete('/concerts/:id', (req, res) => {
+  const id = req.params.id-1;
+  testimonials.splice(concerts[id], 1);
+  res.json({ message: 'OK' });
+});
+
+app.put('/concerts/:id', (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const id = req.params.id-1;
+  const concert = concerts[id];
+  concert.performer = performer;
+  concert.genre = genre;
+  concert.price = price;
+  concert.day = day;
+  concert.image = image;
+  res.json({ message: 'OK' });
+});
+
+// SEATS
+app.get('/seats', (req, res) => {
+  res.json(seats)
+});
+
+app.get('/seats/:id', (req, res) => {
+  res.json(seats[req.params.id-1])
+});
+
+app.post('/seats', (req, res) => {
+  const { day, seat, client, email } = req.body;
+  const id = uuid();
+  const newSeat = { id, day, seat, client, email };
+  seats.push(newSeat);
+  res.json({ message: 'OK' });
+});
+
+app.delete('/seats/:id', (req, res) => {
+  const id = req.params.id-1;
+  seats.splice(seats[id], 1);
+  res.json({ message: 'OK' });
+});
+
+app.put('/seats/:id', (req, res) => {
+  const { day, seat, client, email } = req.body;
+  const id = req.params.id-1;
+  const editSeat = seats[id];
+  editSeat.day = day;
+  editSeat.seat = seat;
+  editSeat.client = client;
+  editSeat.email = email;
+  res.json({ message: 'OK' });
+});
+
 
 app.use((req, res) => {
   res.status(404).json('404 not found...');
